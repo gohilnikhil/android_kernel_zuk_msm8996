58b343d9252ed242e49bad4c3f6e8ac2599b9de9
aswin7469 <aswinas7469@gmail.com>
2019-12-23 17:43:26 +0530

    Revert "simple_lmk: Introduce Simple Low Memory Killer for Android"
    This reverts commit 9de42e5fce3a7a30bd169a52c14bd93bcd0dad5f.

--- kernel/fork.c
+++ kernel/fork.c
@@ -79,10 +79,6 @@
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
 #include <linux/cpufreq_times.h>
-#include <linux/cpufreq.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/boost_control.h>
-#include <linux/simple_lmk.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -730,7 +726,6 @@ static inline void __mmput(struct mm_struct *mm)
 	}
 	if (mm->binfmt)
 		module_put(mm->binfmt->module);
-	simple_lmk_mm_freed(mm);
 	mmdrop(mm);
 }
 
