c2e07aa1ec6877759b53f6f6e4062831725f335c
zeta96 <ahmad.zainul857@gmail.com>
2020-03-03 01:21:21 +0700

    printk: Silence init logs
    Taken from here https://github.com/YaroST12/RAPHAEL_Kernel/commit/41cae4a1e942c33228f080df70c8b13ae9f2feba
    
    It's very annoying
    
    init: Received control message 'interface_start' for 'vendor.lineage.camera.motor@1.0::ICameraMotor/default' from pid: 326 (/system/bin/hwservicemanager)
    init: Could not find 'vendor.lineage.camera.motor@1.0::ICameraMotor/default' for ctl.interface_start
    init: Received control message 'interface_start' for 'android.hardware.graphics.mapper@3.0::IMapper/default' from pid: 326 (/system/bin/hwservicemanager)
    init: Could not find 'android.hardware.graphics.mapper@3.0::IMapper/default' for ctl.interface_start
    
    Some ROM‌ components have been missed here, pretty sure a ROM developer
    can fix it.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -659,6 +659,8 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 			endp++;
 			len -= endp - line;
 			line = endp;
+                        if (strstr(line, "init"))
+				goto free;
 		}
 	}
 
@@ -668,7 +670,8 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 	}
 
 	printk_emit(facility, level, NULL, 0, "%s", line);
-	kfree(buf);
+free:  
+        kfree(buf);
 	return ret;
 }
 
